128aa10fb3867d36dd19adffe69b4814821be4ca
Ranjit kumar yadav <ranjit1435yadav@gmail.com>
2026-08-11 15:31:30 +0530

    dashboard1

--- first.cpp
+++ first.cpp
@@ -1,11 +1,13 @@
 #include<iostream>
 using namespace std;
 int main (){
-    int n=20;
-    int sum=0;
+    int n=6;
     for(int i=0; i<n; i++){
-        sum=sum+i;
-        cout<<sum<<endl;
+       for (int i = 0; i<n; i++)
+       {
+        cout<<"*";
+       }
+       
 
     }
     return 0;
