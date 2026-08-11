26db690d3ea6ba184f7dd00b035ff595ec28ca21
Ranjit kumar yadav <ranjit1435yadav@gmail.com>
2026-08-11 15:28:28 +0530

    dashboard code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 using namespace std;
 int main (){
-    int n=4;
-    cout<<n<<endl;
+    int n=5;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<"*";
+        }
+    }
     return 0;
 }
